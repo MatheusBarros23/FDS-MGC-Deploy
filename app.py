7d6e25cdc042168b73ee94e85df7a4b29a2b880a
Matheus Barros <88664949+MatheusBarros23@users.noreply.github.com>
2023-02-17 14:19:09 -0300

    Correct behavior: First execute prepara_banco before app.py For that, need to implement a function to import

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_mysqldb import MySQL
 
+from prepara_banco import prepara_banco
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -8,5 +9,9 @@ db = MySQL(app)
 
 from views import *
 
+#executo o bd antes!
+prepara_banco()
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
+
